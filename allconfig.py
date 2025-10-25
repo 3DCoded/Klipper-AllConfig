22ce3ec542f8cff0ca500d500ac7a70c2e1aa78b
3DCoded <info3dcoded@gmail.com>
2025-10-25 08:39:32 -0400

    feat: Add section paths to allconfig.cfg

--- allconfig.py
+++ allconfig.py
@@ -13,6 +13,11 @@
 
 import textwrap
 from pathlib import Path
+import configparser
+from io import StringIO
+import glob
+import os
+import logging
 
 INDENT = ' '*4
 HEADER = """
@@ -67,20 +72,80 @@ HEADER = """
 
 """
 
+class ConfigParser:
+    def __init__(self, printer):
+        global config_path
+        self.printer = printer
+
+        self.config_file = printer.start_args['config_file']
+        self.config_path = Path(os.path.dirname(self.config_file))
+        config_path = self.config_path
+
+    def read_config(self):
+        section_filenames = self._read_file(self.config_file)
+        return section_filenames
+
+    def _read_file(self, filename, visited=[], section_filenames={}):
+        path = self.config_path / filename
+        visited.append(path) # Keep a list of files previously included
+        with open(path, 'r') as file:
+            parser = configparser.RawConfigParser(
+            strict=False, inline_comment_prefixes=(';', '#'))
+            parser.read_file(file, filename)
+            for section in parser.sections():
+                if section in section_filenames:
+                    section_filenames[section].append(filename)
+                else:
+                    section_filenames[section] = [filename]
+
+            file.seek(0)
+
+            for line in file.readlines():
+                mo = configparser.RawConfigParser.SECTCRE.match(line)
+                header = mo and mo.group('header')
+                # Handle [include xxx.cfg] sections
+                if header and header.startswith('include '):
+                    include_spec = header[8:].strip()
+                    include_path = str(path.parent / include_spec)
+                    include_filenames = glob.glob(include_path, recursive=True)
+                    for include_filename in include_filenames:
+                        self._read_file(include_filename, visited, section_filenames)
+
+        visited.remove(path)
+
+        return section_filenames
+
 class PrinterAllConfig:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.config = config
 
-        cfg_folder = Path(self.printer.start_args['config_file']).parent
-        self.output_path = Path(config.get('output', str(cfg_folder / 'allconfig.cfg')))
+        self.cfg_folder = Path(self.printer.start_args['config_file']).parent
+        self.output_path = Path(config.get('output', str(self.cfg_folder / 'allconfig.cfg')))
+
+        self.cfg_files = []
+        self.section_sources = {}
 
         self.printer.register_event_handler('klippy:connect', self.handle_connect)
 
     def handle_connect(self):
-        allconfig = ''+HEADER
+        section_filenames = ConfigParser(self.printer).read_config()
+        cfg_root = str(self.cfg_folder) + '/'
+        for section, files in section_filenames.items():
+            section_filenames[section] = [
+                f[len(cfg_root):] if f.startswith(cfg_root) else f
+                for f in files
+    ]
+
+        allconfig = ''+HEADER+str(section_filenames)
         for section in self.config.fileconfig.sections():
-            allconfig += f'[{section}]\n'
+            allconfig += f'[{section}]'
+            if section in section_filenames:
+                sources = section_filenames[section]
+                source_str = ', '.join(sources)
+                allconfig += f'  # {source_str}'
+            allconfig += '\n'
+
             for option, value in self.config.fileconfig.items(section):
                 if len(value.splitlines()) == 1:
                     allconfig += f'{option}: {value}\n'
